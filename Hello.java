10a1235c53a35f4dc38f634ea9f325a858ccd17b
Raaj14Git <raajboopathy@gmail.com>
2025-05-02 11:39:08 +0530

    add Hello.class and java

---
+++ Hello.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Hello{
+
+  public static void main(String args [])
+  {
+Scanner scan = new Scanner(System.in);
+int a = scan .nextInt();
+
+
+if (a %3 == 0 && a%5 ==0)
+System.out.println("a is devide by both 3 and 5");
+
+else if (a%3 == 0)
+System.out.println("A is devide by 3 only");
+
+else 
+System.out.println("Ais devide by 5 only");
+    
+
+//System.out.println(res);
+  }
+}
