aa881c4c8fac1474efadb1436ab5a90e3facc6a8
Raaj14Git <raajboopathy@gmail.com>
2025-05-06 22:07:06 +0530

    turnary operator

--- Hello.java
+++ Hello.java
@@ -26,9 +26,20 @@ break;
 case 2:
 System.out.println ("case" + a);
   break;
+  default:
+  System.out.println("enter value between 1&2 ");
 }
+
+String result = a>1 ? "yes":"no";
     
 
-//System.out.println(res);
+System.out.println(result);
+
+
+
+boolean rain = false;
+String result1 = rain?"bring umbrella" :"enjoy sun light";
+System.out.println(result1);
+
   }
 }
