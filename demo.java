a03e5479135808cf9ca53aeb69b893b1471699e4
Raaj14Git <raajboopathy@gmail.com>
2025-05-13 22:44:56 +0530

    function

---
+++ demo.java
@@ -0,0 +1,24 @@
+public class demo {
+    
+void greating()
+{
+
+    System.out.println("vanakkam");
+dummy();
+}
+
+
+void dummy(){
+
+    System.out.println("dummy");
+}
+public static void main (String args []){
+
+
+    demo obj1 = new demo();
+obj1.greating();
+//obj1.dummy();
+
+}
+
+}
---
+++ mobile.java
@@ -0,0 +1,25 @@
+public class mobile {
+void iphone(){
+System.out.println("this is iphone");
+}
+
+void samsung(){
+
+    System.out.println("thi is samsung");
+}
+
+void vivo(){
+
+    System.out.println("this is vivo");
+}
+
+
+    public static void main(String[] args) {
+      mobile mob1 = new mobile();
+      mob1.iphone();
+      mob1.samsung();
+      mob1.vivo();    
+
+
+    }
+}
