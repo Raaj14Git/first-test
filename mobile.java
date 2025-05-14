eb1176b4b5bed5fe4893fa237c9c604e82790b26
Raaj14Git <raajboopathy@gmail.com>
2025-05-14 08:59:10 +0530

    function with mobile example

--- mobile.java
+++ mobile.java
@@ -1,25 +1,19 @@
 public class mobile {
-void iphone(){
-System.out.println("this is iphone");
+int a =20 , b=5 ;
+void total_money (){
+
+    System.out.println(a*b);
 }
 
-void samsung(){
-
-    System.out.println("thi is samsung");
-}
-
-void vivo(){
-
-    System.out.println("this is vivo");
-}
-
-
+   
     public static void main(String[] args) {
-      mobile mob1 = new mobile();
-      mob1.iphone();
-      mob1.samsung();
-      mob1.vivo();    
+        
+
+        mobile obj1 = new mobile();
+        obj1.total_money();
+
+    }  
 
 
-    }
+    
 }
